56eb6a09d7edce17204b401c58caf4efc9208ddd
Leos03 <112361666+Leos03@users.noreply.github.com>
2022-09-12 21:36:58 -0600

    Update Class2.cs
    oh heccs

--- Class2.cs
+++ Class2.cs
@@ -1,8 +1,32 @@
-﻿using System;
+using System;
 
-public class Class1
+public class Program
 {
-	public Class1()
-	{
-	}
+    public void Main()
+    {
+        Console.WriteLine("Oh ell there");
+        DoMath(14, 2);
+        DoMath(21, 5);
+        DoMath(42, 15);
+        Compare(4, 3);
+        Compare(3, 4);
+    }
+
+    public void DoMath(int value, int value2)
+    {
+        var number = value + value2;
+        Console.WriteLine(number);
+    }
+
+    public void Compare(int value, int value2)
+    {
+        if (value > value2)
+        {
+            Console.WriteLine("True. the first is greater.");
+        }
+        else
+        {
+            Console.WriteLine("False, the second is greater.");
+        }
+    }
 }
