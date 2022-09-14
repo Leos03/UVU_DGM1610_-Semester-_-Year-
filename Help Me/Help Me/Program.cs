7215bcf7e5945dae95d550986ff18a8b9a664ada
Leos <10955439@uvu.edu>
2022-09-13 21:39:21 -0600

    Why am I allowed to commit

--- Help Me/Help Me/Program.cs
+++ Help Me/Help Me/Program.cs
@@ -12,15 +12,20 @@ public class Program
         Compare(3, 4);
     }
 
-    public void DoMath (int value, int value2) {
+    public void DoMath(int value, int value2)
+    {
         var number = value + value2;
         Console.WriteLine(number);
     }
 
-    public void Compare (int value, int value2) { 
-        if (value > value2) {
+    public void Compare(int value, int value2)
+    {
+        if (value > value2)
+        {
             Console.WriteLine("True. the first is greater.");
-        } else {
+        }
+        else
+        {
             Console.WriteLine("False, the second is greater.");
         }
     }
